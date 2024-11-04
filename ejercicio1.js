338b9e447c3c150847732055db3fc52d16f38a58
Miguel <migue2801224@gmail.com>
2024-11-04 17:23:51 -0500

    Primer commit

---
+++ ejercicio1.js
@@ -0,0 +1,4 @@
+//Ejercicio 1: Gestión de Inventario de Tienda
+//Descripción: Crea un objeto que represente una tienda con varios productos. Cada producto debe tener propiedades como nombre, precio y cantidad. Luego, crea funciones para agregar productos, actualizar la cantidad de productos y mostrar la información de todos los productos.
+
+const productos = ["huevos", "gaseosa", "azucar", "arroz"]
